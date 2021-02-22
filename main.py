a0fa80e0aaa14ff19e9c3f1aa0b9538e4a88cca9
ArSiu <arsvikh@gmail.com>
2021-02-22 10:49:14 +0200

    amendments to the rules of writing code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class Shoes:
-    #static var
+    # static var
     shoe_popular = 0
 
-    #constructor
-    def __init__(self,shoeBrand = "",shoeCountryManufactoty = "",shoeCollection = "",shoePrice = 0,shoeSize = 0,shoeColor = ""):
+    # constructor
+    def __init__(self, shoeBrand="", shoeCountryManufactoty="", shoeCollection="", shoePrice=0, shoeSize=0, shoeColor=""):
         print("star production of shoes")
         self.shoeBrand = shoeBrand
         self.shoeCountryManufactoty = shoeCountryManufactoty
@@ -13,23 +13,33 @@ class Shoes:
         self.shoeColor = shoeColor
         Shoes.shoe_popular += 1
 
-    #destructor
+    # destructor
     def __del__(self):
         pass
 
     def __str__(self):
         Shoes.shoe_popular += 1
-        return f"\nBrand: {self.shoeBrand}\n Country: {self.shoeCountryManufactoty}\n Collection: {self.shoeCollection}\n Price: {self.shoePrice}\n Size: {self.shoeSize}\n Color: {self.shoeColor}\n Popularity: {Shoes.shoe_popular}\n"
+        return f"\nBrand: {self.shoeBrand}\n " \
+               f"Country: {self.shoeCountryManufactoty}\n " \
+               f"Collection: {self.shoeCollection}\n " \
+               f"Price: {self.shoePrice}\n " \
+               f"Size: {self.shoeSize}\n " \
+               f"Color: {self.shoeColor}\n " \
+               f"Popularity: {Shoes.shoe_popular}\n"
 
-    #methonds
+    # methonds
     @staticmethod
     def getShabbiness():
         return Shoes.shoe_shabbiness
 
+
 def main():
-    shoes = [Shoes("Abibas","USA",12,40,"Sky blue"),Shoes("Nike","USA",30,41,"Dark rock"),Shoes("Abraham","USA",50,42,"Кed copper")]
+    shoes = [Shoes("Abibas", "USA", 12, 40, "Sky blue"),
+             Shoes("Nike", "USA", 30, 41, "Dark rock"),
+             Shoes("Abraham", "USA", 50, 42, "Кed copper")]
     for shoe in shoes:
         print(shoe)
+
 
 if __name__ == "__main__":
     main()
