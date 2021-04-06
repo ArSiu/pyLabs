c6739df7b70ac124dcf0e8db95b9cb376734d3d4
ArSiu <arsvikh@gmail.com>
2021-04-06 11:03:20 +0300

    Code review.Marshmallow validation and 404 abort if res does not exist. Also division of everything into different modules

--- database/__init__.py
+++ database/__init__.py
@@ -1,2 +1,1 @@
-from .db import Technique, Companies, TechniqueSchema, technique_schema, techniques_schema, db, ma
-
+from .db import Technique, Companies, TechniqueSchema, technique_schema, techniques_schema, db, ma, create_db
--- database/db.py
+++ database/db.py
@@ -1,6 +1,10 @@
+from os.path import dirname, join, abspath
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import validate, fields, post_load
+from marshmallow_enum import EnumField
 from enum import Enum
+
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -17,7 +21,7 @@ class Technique(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     model = db.Column(db.String(20), nullable=False)
-    manufactory_company = db.Column(db.Enum(Companies), nullable=False)
+    manufactory_company = db.Column(db.Enum(Companies))
     year_of_manufacture = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)
     input_voltage = db.Column(db.Float, nullable=False)
@@ -40,10 +44,31 @@ class Technique(db.Model):
 
 
 class TechniqueSchema(ma.Schema):
-    class Meta:
-        fields = ('name', 'model', ' manufactory_company', 'year_of_manufacture', 'price', 'input_voltage',
-                  'watts')
+    # зефірка валідація
+    name = fields.Str(validate=validate.Length(min=1, max=32))
+    model = fields.Str(validate=validate.Length(min=1, max=20))
+    manufactory_company = EnumField(Companies)
+    year_of_manufacture = fields.Int(validate=validate.Range(min=1900, max=9999))
+    price = fields.Float(validate=validate.Range(min=0.0, max=9999.999))
+    input_voltage = fields.Float(validate=validate.Range(min=0.0, max=340.0))
+    watts = fields.Int(validate=validate.Range(min=0, max=9999))
+
+    # піздец важлива хрень десиріалізація з зефірки в пітон
+    @post_load
+    def make_technique(self, data, **kwargs):
+        return Technique(**data)
 
 
 technique_schema = TechniqueSchema()
 techniques_schema = TechniqueSchema(many=True)
+
+
+def create_db(app):
+    try:
+        db.create_all(app=app)
+    except Exception as e:
+        print(e)
+        print("CANNOT CONNECT TO DB(CHECK config.py SQLALCHEMY_DATABASE_URI IF THIS FIELD IS OK) CREATING SQLITE DB")
+        dbdir = join(dirname(dirname(abspath(__file__))), "database")
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + join(dbdir, 'db.sqlite')
+        db.create_all(app=app)
